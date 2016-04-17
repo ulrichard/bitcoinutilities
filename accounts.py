457f3bd882bc9952b97e22b72be88b832182b2ac
Richard Ulrich <richi@paraeasy.ch>
2016-04-17 20:38:17 +0200

    trying to get transactions for the case hardware wallet - so far without success

--- accounts.py
+++ accounts.py
@@ -30,26 +30,33 @@ class accounts:
                     kk1 = pycoin.key.BIP32Node.BIP32Node.from_hwif(addr[1])
                     kk2 = pycoin.key.BIP32Node.BIP32Node.from_hwif(addr[2])
 
-                    for j in range(2): # receive and change addresses
-                        gap = 0
-                        for i in range(99999):
-                            keypath = "%d/%d.pub" % (j, i)
-                            #print(keypath)
-                            sub0 = kk0.subkey_for_path(keypath).sec()
-                            sub1 = kk1.subkey_for_path(keypath).sec()
-                            sub2 = kk2.subkey_for_path(keypath).sec()
-                            sub = sorted([sub0, sub1, sub2])
-                            underlying_script = ScriptMultisig(n=2, sec_keys=[sub[0], sub[1], sub[2]]).script()
-                            addr = address_for_pay_to_script(underlying_script, netcode="BTC")
-                            ledger = blockchain_info.blockchain(addr, False)
-                            bal  = ledger.balance()
-                            print desc, i, j, addr, bal
-                            balance[name][addr] = [bal, '%s_%s_%d' % (desc, 'P' if 0 == j else 'Chg', i)]
-                            if bal == 0:
-                                if 0 == ledger.tx_count():
-                                    gap += 1
-                                    if gap > 10:
-                                        break
+                    for k in range(4): # device (0), web (1) or exchange (2) for case hardware wallet. idx (3) used for non-case
+                        for j in range(2): # receive (0) and change (1) addresses
+                            gap = 0
+                            for i in range(99999):
+                                if k == 3:   # regular
+                                    keypath = "%d/%d.pub" % (j, i)
+                                else:        # case
+                                    keypath = "%d/%d/%d.pub" % (k, j, i)
+                                #print(keypath)
+                                sub0 = kk0.subkey_for_path(keypath).sec()
+                                sub1 = kk1.subkey_for_path(keypath).sec()
+                                sub2 = kk2.subkey_for_path(keypath).sec()
+                                if k == 3: # regular
+                                    sub = sorted([sub0, sub1, sub2])
+                                else:      # case
+                                    sub = [sub0, sub1, sub2]
+                                underlying_script = ScriptMultisig(n=2, sec_keys=[sub[0], sub[1], sub[2]]).script()
+                                addr = address_for_pay_to_script(underlying_script, netcode="BTC")
+                                ledger = blockchain_info.blockchain(addr, False)
+                                bal  = ledger.balance()
+                                #print desc, i, j, addr, bal
+                                balance[name][addr] = [bal, '%s_%s_%d' % (desc, 'P' if 0 == j else 'Chg', i)]
+                                if bal == 0:
+                                    if 0 == ledger.tx_count():
+                                        gap += 1
+                                        if gap > 10:
+                                            break
                 elif len(addr) < 40:
                     # regular address
                     ledger = blockchain_info.blockchain(addr, False)
