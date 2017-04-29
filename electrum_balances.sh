789ffde4e35cb4536d9408e0146cad9a50e43dd6
Richard Ulrich <richi@paraeasy.ch>
2017-04-29 09:50:56 +0200

    balance

--- electrum_balances.sh
+++ electrum_balances.sh
@@ -1,9 +1,24 @@
-#! /bin/sh
+#! /bin/bash
 
-#!/bin/bash
-FILES=~/.electrum/wallets/*
-for f in $FILES
+totalchf=0
+btcchf=$(curl -s https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=CHF | jq -r '.[0].price_chf')
+echo $btcchf
+
+printf "%25s\t%10s\t%8s\n" file BTC CHF
+
+for f in ~/.electrum/wallets/*
 do
-  echo "Processing $f file..."
-  electrum getbalance -w $f 
+    if [ -f "$f" ]
+    then
+        balance=$(electrum getbalance -w $f | jq -r '.confirmed')
+        chf=$(echo "$balance $btcchf * p" | dc)
+        printf "%25s\t%.8f\t%'.2f\n" $(basename $f) $balance $chf
+
+        if [ "family_and_friends" != "$(basename $f)" ]
+        then
+            totalchf=$(echo "$totalchf $chf + p" | dc)
+        fi
+    fi
 done
+
+printf "total: %'.2f\n" $totalchf
