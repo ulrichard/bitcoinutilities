bea4ac7ce4cf25655885f26eb21578ad84ca7967
Richard Ulrich <richi@paraeasy.ch>
2020-12-21 17:12:09 +0100

    adapted for electrum 4

--- electrum_balances.sh
+++ electrum_balances.sh
@@ -5,23 +5,27 @@ prices=$(curl ${CURL_PROXY} -H "X-CMC_PRO_API_KEY: 67cb5457-a155-47d8-8a36-1c818
 btcchf=$(echo $prices | jq -r '.data[] | select(.symbol=="BTC") | .quote.CHF.price')
 echo BTC-CHF: $btcchf
 
+electrum daemon -d
+
 printf "%25s\t%10s\t%8s\n" file BTC CHF
 
 for f in ~/.electrum/wallets/*
 do
     if [ -f "$f" ]
     then
-        electrum daemon load_wallet -w $f > /dev/null
+        electrum load_wallet -w $f > /dev/null
         balance=$(electrum getbalance -w $f | jq -r '.confirmed')
         chf=$(echo "$balance $btcchf * p" | dc)
         printf "%25s\t%.8f\t%'.2f\n" $(basename $f) $balance $chf
 
-        if [ "family_and_friends" != "$(basename $f)" ] && [  "Mirella" != "$(basename $f)" ]
+        if [ "family_and_friends" != "$(basename $f)" ] 
         then
             total=$(echo "$total $balance + p" | dc)
         fi
     fi
 done
+
+electrum stop
 
 chf=$(echo "$total $btcchf * p" | dc)
 printf "\n%25s\t%.8f\t%'.2f\n" total $total $chf
